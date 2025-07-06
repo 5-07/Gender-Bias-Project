9dd83126319f8ae5f2cbc51e6764f39a9e19356e
5-07 <muzainamunirsiddiqui@gmail.com>
2025-07-07 01:13:53 +0400

    added testing code for original bios

--- bias_analysis.py
+++ bias_analysis.py
@@ -2,3 +2,17 @@ import pandas as pd
 
 df_pairs = pd.read_csv("bios_pairs.csv")
 print(df_pairs.head())
+#To test on og bios
+orig_encodings = tokenizer(
+    df_pairs["original_text"].tolist(),
+    truncation=True,
+    padding=True,
+    max_length=256,
+    return_tensors="pt",
+)
+
+model.eval()
+with torch.no_grad():
+    outputs_orig = model(**orig_encodings)
+    preds_orig = outputs_orig.logits.argmax(dim=-1).numpy()
+    conf_orig = outputs_orig.logits.softmax(dim=-1).max(dim=-1).values.numpy()
