3eb962687f34bc10b51fd709685f2242fc35ea7c
5-07 <muzainamunirsiddiqui@gmail.com>
2025-07-07 01:15:27 +0400

    added code o test on cf bios

--- bias_analysis.py
+++ bias_analysis.py
@@ -16,3 +16,17 @@ with torch.no_grad():
     outputs_orig = model(**orig_encodings)
     preds_orig = outputs_orig.logits.argmax(dim=-1).numpy()
     conf_orig = outputs_orig.logits.softmax(dim=-1).max(dim=-1).values.numpy()
+
+#To test on cf bios
+cf_encodings = tokenizer(
+    df_pairs["counterfactual_text"].tolist(),
+    truncation=True,
+    padding=True,
+    max_length=256,
+    return_tensors="pt",
+)
+
+with torch.no_grad():
+    outputs_cf = model(**cf_encodings)
+    preds_cf = outputs_cf.logits.argmax(dim=-1).numpy()
+    conf_cf = outputs_cf.logits.softmax(dim=-1).max(dim=-1).values.numpy()
